7e7b646c385b8f8c3cfa71efd814b31f65d79664
Manik Chawla <69040146+manikchawla12@users.noreply.github.com>
2023-10-04 16:47:48 +0530

    admincommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginCompComponent } from './login-comp/login-comp.component';
 import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './AdminComponents/dashboard/dashboard.component';
+import { TestsComponent } from './AdminComponents/tests/tests.component';
+import { TestCenterComponent } from './AdminComponents/test-center/test-center.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginCompComponent },
-  { path: 'register', component: RegisterComponent}
+  { path:"login", component: LoginCompComponent },
+  { path:"register", component: RegisterComponent},
+  { path:"dashboard",component:DashboardComponent},
+  { path:"tests", component:TestsComponent},
+  { path:"testCenter", component:TestCenterComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { LoginCompComponent } from './login-comp/login-comp.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 
+import { DashboardComponent } from './AdminComponents/dashboard/dashboard.component';
+import { TestsComponent } from './AdminComponents/tests/tests.component';
+import { AdminNavbarComponent } from './AdminComponents/admin-navbar/admin-navbar.component';
+import { TestCenterComponent } from './AdminComponents/test-center/test-center.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginCompComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+ 
+    DashboardComponent,
+    TestsComponent,
+    AdminNavbarComponent,
+    TestCenterComponent,
   ],
   imports: [
     BrowserModule,
