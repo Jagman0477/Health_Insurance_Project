e3fe8d4a705c60e23f7182233994e707ba4e456f
Jagman0477 <76586290+Jagman0477@users.noreply.github.com>
2023-10-09 00:33:53 +0530

    random checkout error

--- src/app/register/registerData.ts
+++ src/app/register/registerData.ts
@@ -1,10 +1,19 @@
 export class registerData{
 
-    name:string = "";
-    age:number = 0;
-    gender:string = "";
-    email:string = "";
-    password:string = "";
-    confirmPassword:string = "";
+    name:string;
+    age:number;
+    gender:string;
+    email:string;
+    password:string;
+    confirmPassword:string;
+
+    constructor(){
+        this.name = '';
+        this.age = 0;
+        this.gender = '';
+        this.email = '';
+        this.password = '';
+        this.confirmPassword = '';
+    }
 
 }
