7fff32a654e6ed15a83373b309bd512d8c51b82c
Jagman0477 <76586290+Jagman0477@users.noreply.github.com>
2023-10-20 09:48:26 +0530

    Patient list files

--- src/app/AdminComponents/patients-list/patients-list.component.ts
+++ src/app/AdminComponents/patients-list/patients-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { registerData } from 'src/app/register/registerData';
+import { PatientListServiceService } from 'src/app/services/patientList/patient-list-service.service';
 
 @Component({
   selector: 'app-patients-list',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./patients-list.component.css']
 })
 export class PatientsListComponent {
+
+  patientList: registerData[] | undefined; 
+  isPatientListEmpty = new EventEmitter<boolean>(false);
+  patientListError: string = '';
+  
+  constructor(private patients: PatientListServiceService){ }
+
+  ngOnInit(): void{
+    this.refreshPatientList();
+  }
+
+  refreshPatientList(){
+    this.patients.patientList().subscribe((result: any) => {
+      console.log(result);      
+      if(result && result.length){
+        this.patientList = result;
+      }
+      else {
+        this.isPatientListEmpty.emit(true);
+        this.patientListError = "Patient list is empty."
+      }
+    });
+  }
+
+  deletePatient(id: number){
+    this.patients.deletePatient(id).subscribe((result) => {
+      if(result)
+        this.refreshPatientList();
+    })
+  }
 
 }
